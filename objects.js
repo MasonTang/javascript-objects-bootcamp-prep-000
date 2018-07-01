2ee856736d8fad949731d9501e9dd3f55ce7adf7
masontang@aol.com <masontang@aol.com>
2018-07-01 13:33:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {}
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
