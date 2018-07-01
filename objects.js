b2ab34627673cabb4ea14b2a27cf576fa4e490fe
masontang@aol.com <masontang@aol.com>
2018-07-01 13:41:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
 }
+
+updatePlaylist(playlist, "Jay Z", "High")
+updatePlaylist(playlist, "Skillet", "Undefeated")
+removeFromPlaylist(playlist, "Jay Z")
